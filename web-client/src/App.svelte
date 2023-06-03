b41be8ffaf1a00341a2d0a95d90063a1a41490da
Javier Sánchez Rois <javier.sanchez.rois@gmail.com>
2023-06-03 18:55:00 +0200

    Initial commit

---
+++ web-client/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {onMount} from "svelte";
+    import SongsApi from "./api/songsApi.js";
+    import logo from "./assets/logo."
+
+    let songs = [];
+    let api = new SongsApi();
+    let errorMsg;
+
+    onMount(() => {
+        api.getSongs()
+            .then(data => songs = data)
+            .catch(error => errorMsg = error)
+    });
+</script>
+
+<header>
+    <img src={logo}/>
+</header>
