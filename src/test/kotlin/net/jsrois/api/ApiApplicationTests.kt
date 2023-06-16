820dd8d5d113253c1f4a2446ff18a4a4324a65e2
Javi <javi@jsrois.net>
2023-06-16 18:59:19 +0200

    add test for products

--- src/test/kotlin/net/jsrois/api/ApiApplicationTests.kt
+++ src/test/kotlin/net/jsrois/api/ApiApplicationTests.kt
@@ -1,17 +1,40 @@
 package net.jsrois.api
 
-import com.codeborne.selenide.Configuration
-import com.codeborne.selenide.Selenide.`$`
-import com.codeborne.selenide.Selenide.open
+import net.jsrois.api.domain.Product
+import net.jsrois.api.repositories.ProductRepository
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.openqa.selenium.By
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.context.annotation.Profile
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.OK
+import org.springframework.test.context.ActiveProfiles
+import java.util.UUID
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class ApiTests {
+    @Autowired
+    private lateinit var api: TestRestTemplate
 
+    @Autowired
+    private lateinit var productRepository: ProductRepository
+
+    @Test
+    fun `returns all products`() {
+        val products = productRepository.saveAll(listOf(
+                Product("IPhone 9", 799.99, "http://example.com/image.png", UUID.randomUUID()),
+                Product("Toalla de playa", 13.99, "http://example.com/image.png", UUID.randomUUID())
+        ))
+
+        val response = api.getForEntity("/api/products", Array<Product>::class.java)
+
+        assertThat(response.statusCode, equalTo(OK))
+        assertThat(response.body?.asList(), equalTo(products))
+    }
 }
