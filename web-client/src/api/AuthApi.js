20d6b124abed56034a2880289d6e782cfe9aed0a
Javi <javi@jsrois.net>
2023-06-17 13:53:51 +0200

    login through api

--- web-client/src/api/AuthApi.js
+++ web-client/src/api/AuthApi.js
@@ -2,6 +2,7 @@ import {currentUser, loggedIn} from "../store/session.js";
 import {developmentModeOn} from "./developmentMode.js";
 
 function logInUser(name, id) {
+    console.log(name + " " + id);
     loggedIn.update(_ => true);
     currentUser.update(_ => {
         return {
@@ -21,14 +22,23 @@ export class AuthApi {
     login(username, password) {
         return developmentModeOn ? fakeLogin() : fetch("/api/auth/login", {
             method: 'POST',
-            headers: { "Content-Type": "application/json"},
+            headers: {"Content-Type": "application/json"},
             body: JSON.stringify({username, password})
         })
             .then(response => {
                 if (!response.ok) {
                     throw new Error("Unable to login");
                 }
-                logInUser({...response.body})
+                console.log(`response is ${response}`);
+                return response.json()
             })
+            .then(loginInfo => {
+                let {name, id} = loginInfo
+                console.log(`name is ${name}`);
+                console.log(`id is ${id}`);
+
+                logInUser(name, id);
+            })
+            .catch(err => console.log(err))
     }
 }
