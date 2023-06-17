e7aacf01a0800a3eda352d79df01d06cca62fe71
Javi <javi@jsrois.net>
2023-06-17 23:11:56 +0200

    drawing orders

--- web-client/src/lib/pages/MyAccount.svelte
+++ web-client/src/lib/pages/MyAccount.svelte
@@ -14,10 +14,11 @@
     <h1>Bienvenido {$currentUserName} </h1>
     <h2>Tus pedidos</h2>
     {#await api.getOrders($currentUserId) then orders}
-        {@debug orders}
-        <!--{#each orders as {id, products, shippingData, status, totalCost}}-->
-        <!--    #{id} ({status}) with {products.length} products and a total cost of {totalCost}-->
-        <!--{/each}-->
+<!--        [
+{"id":"06bfb49c-d637-4e5a-bb8f-be92d936f235","address":"carrer de muntaner 137, 2-1","city":"Barcelona","country":"Spain","postalCode":"08099","products":["1e35aafe-46b0-470d-82c5-88394e4f9bcc","49ee6207-5eb1-4779-8c35-adf322ee25ab"]}]-->
+        {#each orders as {id, products, shippingData, status, totalCost}}
+            #{id} ({status}) with {products.length} products and a total cost of {totalCost}
+        {/each}
     {/await}
 
     <Button on:click={logout}>Salir</Button>
