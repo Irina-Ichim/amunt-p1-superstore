c1f6023ae5424d9c516ab5d93f79ac35f380d332
Javi <javi@jsrois.net>
2023-06-18 08:01:47 +0200

    Show order info in MyAccount page

---
+++ web-client/src/lib/components/Order.svelte
@@ -0,0 +1,26 @@
+<script>
+    import {products as productsStore} from "../../store/products.js";
+
+    export let id;
+    export let address;
+    export let city;
+    export let country;
+    export let products = [];
+    $: productsInfo = products.map(id => $productsStore.find(e => e.id == id))
+</script>
+
+<article>
+    <section>
+        <h2>Pedido {id}</h2>
+        <p>Envío: {address} - {city} ({country})</p>
+    </section>
+    <section>
+        <h2>Productos:</h2>
+        {#each productsInfo as {imageUrl, name, price}}
+            <article>
+                <img src={imageUrl} alt={name}/>
+                <p>{name} ({price}€)</p>
+            </article>
+        {/each}
+    </section>
+</article>
