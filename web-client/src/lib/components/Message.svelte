990cdaaea54496e567a60cdda9834eb0b1e94770
ikramadam <126071398+ikramadam@users.noreply.github.com>
2023-06-21 12:29:17 +0200

    estilo del mensage

--- web-client/src/lib/components/Message.svelte
+++ web-client/src/lib/components/Message.svelte
@@ -4,3 +4,14 @@
 <div in:fade out:fade>
     <slot/>
 </div>
+
+<style>
+    div {
+        color: #ffa500;
+        font-size: 28px;
+        font-weight: bold;
+        text-align: right;
+        padding: 59px;
+        margin-left: 20px;
+    }
+</style>
