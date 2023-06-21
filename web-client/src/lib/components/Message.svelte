de0ac240c63dafb91a3add4e96bd0d0b57abbb96
ikramadam <126071398+ikramadam@users.noreply.github.com>
2023-06-21 12:52:17 +0200

    estilo del mensage 2

--- web-client/src/lib/components/Message.svelte
+++ web-client/src/lib/components/Message.svelte
@@ -6,12 +6,19 @@
 </div>
 
 <style>
-    div {
-        color: #ffa500;
-        font-size: 28px;
-        font-weight: bold;
-        text-align: right;
-        padding: 59px;
-        margin-left: 20px;
-    }
-</style>
+     div {
+         position: absolute;
+         right: 0;
+         font-size: large;
+         width: 250px;
+         border: 1px solid #ffa500;
+         color: #ffa500;
+         background-color: #fff6bb;
+         font-weight: bold;
+         padding: 10px;
+         text-align: center;
+         z-index: 9999;
+     }
+    </style>
+
+
